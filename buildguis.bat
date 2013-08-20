039f96676d509e9f96fc2b64d695dd7f02e843b0
Villanueva, Raul <raulvlopez@gmail.com>
2013-08-20 18:45:08 -0500

    standarized ui files and their generated python files

--- buildguis.bat
+++ buildguis.bat
@@ -1,3 +1,7 @@
-pyuic4 mainwindow.ui -o mainwindowui.py -x
-pyuic4 classinfo.ui -o classinfo.py -x
-pyuic4 imageeditor.ui -o imageeditor.py -x
+pyuic4 mainwindow.ui -o ui_mainwindow.py -x
+
+pyuic4 classinfo.ui -o ui_classinfo.py -x
+
+pyuic4 imageeditor.ui -o ui_imageeditor.py -x
+
+pyuic4 perceptron.ui -o ui_perceptron.py -x
